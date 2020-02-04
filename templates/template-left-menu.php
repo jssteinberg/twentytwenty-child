a45d5ab8d4fd88756d0944fb55b67e4f0520e9c9
jssteinberg <jssteinberg@pm.me>
2020-02-04 15:51:41 +0100

    Added left menu template (WIP

---
+++ templates/template-left-menu.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Template Name: Left Menu Template
+ * Template Post Type: post, page
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_Child
+ * @since 1.0.2
+ */
+
+get_template_part( 'singular' );
