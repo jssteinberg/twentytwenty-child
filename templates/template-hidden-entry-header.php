e00e87d35d9527c0a7b1b8cc3857a97483b23235
jssteinberg <jssteinberg@pm.me>
2020-02-19 14:49:40 +0100

    Added template where entry header is hidden when singular

---
+++ templates/template-hidden-entry-header.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Template Name: Hidden Entry Header Template
+ * Template Post Type: post, page
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_Child
+ * @since 1.0.2
+ */
+
+get_template_part( 'singular' );
