131a07576779f526fcd3ce194d7bb16087730d8d
jssteinberg <jssteinberg@pm.me>
2020-02-25 12:07:09 +0100

    Added path class to <body> and general CSS fixes

--- header.php
+++ header.php
@@ -20,7 +20,23 @@
 
 	</head>
 
-	<body <?php body_class(); ?>>
+	<?php
+		/* Get the Page Slug to Use as a Body Class, this will only return a value on pages! */
+		$class = '';
+		/* is it a page */
+		if( is_page() ) { 
+			global $post;
+			/* Get an array of Ancestors and Parents if they exist */
+			$parents = get_post_ancestors( $post->ID );
+			/* Get the top Level page->ID count base 1, array base 0 so -1 */ 
+			$id = ($parents) ? $parents[count($parents)-1]: $post->ID;
+			/* Get the parent and set the $class with the page slug (post_name) */
+			$parent = get_post( $id );
+			$class = 'path-' . $parent->post_name;
+		}
+	?>
+
+	<body <?php body_class( $class ); ?>>
 
 		<?php
 		wp_body_open();
